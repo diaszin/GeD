4b96b695632cf7915716ffbc203ed7b74569e0d4
diaszin <84644190+diaszin@users.noreply.github.com>
2025-10-31 19:11:49 -0300

    Realiza configuração inicial com o Spring Security

---
+++ server/src/main/java/com/projetofinal/ged/infra/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.projetofinal.ged.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                    authorize -> authorize
+                            .requestMatchers("/**").permitAll() // permite TODAS as rotas e métodos
+                            .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+}
