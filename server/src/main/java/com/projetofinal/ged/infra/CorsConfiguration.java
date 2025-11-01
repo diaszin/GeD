1e83c06445f053c12c29131d5841c5ba4c0c70d6
diaszin <84644190+diaszin@users.noreply.github.com>
2025-11-01 15:01:31 -0300

    Add configuração de CORS

---
+++ server/src/main/java/com/projetofinal/ged/infra/CorsConfiguration.java
@@ -0,0 +1,14 @@
+package com.projetofinal.ged.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("*");
+    }
+}
