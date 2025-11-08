dd36ec87df3a528c379d639cae62df900923122f
diaszin <84644190+diaszin@users.noreply.github.com>
2025-11-08 18:50:46 -0300

    Substitui anotação de handler de exceção

--- server/src/main/java/com/projetofinal/ged/infra/UserGlobalExceptionHandler.java
+++ server/src/main/java/com/projetofinal/ged/infra/UserGlobalExceptionHandler.java
@@ -5,8 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class UserGlobalExceptionHandler {
     @ExceptionHandler(UserNotFound.class)
     public ResponseEntity<DefaultExceptionResponse> userNotFound(UserNotFound error){
