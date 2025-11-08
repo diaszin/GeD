74580af40758155e766d44e38c98fb1db8977b2f
diaszin <84644190+diaszin@users.noreply.github.com>
2025-11-08 16:19:06 -0300

    Cria funcionalidade de criaçlão de projeto

--- app/src/api/Project.ts
+++ app/src/api/Project.ts
@@ -1,15 +1,24 @@
+import { AuthToken } from "@/config/AuthToken";
 import axios from "axios";
 
 export class Project {
   private static baseURL: string = import.meta.env.VITE_BACKEND_URL;
+  private static readonly token = AuthToken.get();
 
   public static async create(title: string) {
     const url = `${this.baseURL}/project`;
+    
 
+    const response = await axios.post(
+      url,
+      { title },
+      {
+        headers: {
+          Authorization: "Bearer " + this.token,
+        },
+      }
+    );
 
-
-    const response = await axios.post(url, { title });
-
-    return response
+    return response;
   }
 }
