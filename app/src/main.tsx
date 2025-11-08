f6b3eadec32caa183515aec37704f4f3cac08f24
diaszin <84644190+diaszin@users.noreply.github.com>
2025-11-08 17:50:59 -0300

    Add react query ao projeto

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,5 +1,19 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Routes } from "@generouted/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById("root")!).render(<Routes />);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <Routes />
+  </QueryClientProvider>
+);
