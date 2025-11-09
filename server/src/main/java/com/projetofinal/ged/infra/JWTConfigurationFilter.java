c33917d75e3ec755925b79773ec8a20f5d8822f8
diaszin <84644190+diaszin@users.noreply.github.com>
2025-11-09 14:55:36 -0300

    Refatora configuração de autenticação

--- server/src/main/java/com/projetofinal/ged/infra/JWTConfigurationFilter.java
+++ server/src/main/java/com/projetofinal/ged/infra/JWTConfigurationFilter.java
@@ -3,38 +3,48 @@ package com.projetofinal.ged.infra;
 import com.projetofinal.ged.ports.AuthServicePort;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
 
 @Component
-public class JWTConfigurationFilter implements Filter {
+public class JWTConfigurationFilter extends OncePerRequestFilter {
     private final AuthServicePort jwtAuth;
+    private final HandlerExceptionResolver resolver;
 
-    public JWTConfigurationFilter(AuthServicePort jwtAuth) {
+    public JWTConfigurationFilter(AuthServicePort jwtAuth, @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
         this.jwtAuth = jwtAuth;
+        this.resolver = resolver;
     }
 
+
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String header = request.getHeader("Authorization");
 
-        if (header != null && header.startsWith("Bearer")) {
-            String token = header.split(" ")[1];
+        try {
+            if (header != null && header.startsWith("Bearer")) {
+                String token = header.split(" ")[1];
 
-            if (jwtAuth.validate(token)) {
-                String email = jwtAuth.getEmail(token);
+                if (jwtAuth.validate(token)) {
+                    String email = jwtAuth.getEmail(token);
 
-                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(email, null, null);
+                    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(email, null, null);
 
-                SecurityContextHolder.getContext().setAuthentication(auth);
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
             }
-        }
 
-        filterChain.doFilter(servletRequest, servletResponse);
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            resolver.resolveException(request, response, null, e);
+        }
     }
 }
--- server/src/main/java/com/projetofinal/ged/infra/SecurityConfiguration.java
+++ server/src/main/java/com/projetofinal/ged/infra/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,6 +21,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http, JWTConfigurationFilter jwtConfigurationFilter) throws Exception {
         http
                 .cors(cors -> corsSecurityConfiguration.corsConfigurationSource())
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                     authorize -> authorize
